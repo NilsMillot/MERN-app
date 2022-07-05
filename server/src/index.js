ff720aab064e339fc1325b87232d7f5ad404dc03
NilsMillot <compte.mxs@hotmail.fr>
2022-07-05 20:06:28 +0200

    [ADD] add express works good

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,11 @@
-console.log("dans le server src/");
+const express = require("express");
+const port = 5000;
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res, next) => {
+  res.send("Hello wooorld!");
+});
+
+app.listen(port, () => console.log(`Server started ${port}`));
