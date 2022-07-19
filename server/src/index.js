9fdc863a0bbf7f6c794918c71aeb9294b3f32d3f
NilsMillot <compte.mxs@hotmail.fr>
2022-07-19 17:17:33 +0200

    [FEAT] add sequalize and connexion logic

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const port = 5000;
 const app = express();
+const SecurityRouter = require("./routes/Security");
 
 app.use(express.json());
 
 app.get("/", (req, res, next) => {
   res.send("Hello wooorld!");
 });
+
+app.use(SecurityRouter);
 
 app.listen(port, () => console.log(`Server started ${port}`));
