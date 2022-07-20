bc044578dd292f0fe77acd4e8d28276fd5b9876e
Nils <nils.millot@canal-plus.com>
2022-07-20 19:53:04 +0200

    add navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import { Route, Routes } from "react-router-dom";
+import NavBar from "./Components/NavBar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
-      <h1>Welcome to React Router!</h1>
+      <NavBar />
       <Routes>
         <Route path="/" element={<h1>home</h1>} />
         <Route path="about" element={<h1>login</h1>} />
