d74196d694e31e248e5fe776ce154d2f21b62909
Sidox <pro.sidox@gmail.com>
2022-07-28 13:39:43 +0200

    static view chat

--- client/src/Components/chat/Messaging.jsx
+++ client/src/Components/chat/Messaging.jsx
@@ -1,15 +1,54 @@
 import React from "react";
 import { AuthContext } from "../../App";
-import ChatHeader from "./Chat_Header";
-
+import ChatHeader from "./chatHeader/Chat_Header";
+import Message from "./messages/Message";
+import Conversation from "./conversations/Conversation";
+import "./messaging.scss";
+import ChatOnline from "./chatOnline/ChatOnline";
 export default function HomePage() {
   //   const { token } = React.useContext(AuthContext);
   const auth = React.useContext(AuthContext);
 
   return (
     <>
-      <ChatHeader/>
-      <div>Authenticated as {auth?.email}</div>
+      <ChatHeader />
+      {/*<div>Authenticated as {auth?.email}</div>*/}
+      <div className="messenger">
+        <div className="chatMenu">
+          <div className="chatMenuWrapper">
+            <input placeholder="Search for friends" className="chatMenuInput" />
+            <Conversation />
+          </div>
+        </div>
+        <div className="chatBox">
+          <div className="chatBoxWrapper">
+            <div className="chatBoxTop">
+              <Message />
+              <Message own={true} />
+              <Message />
+              <Message own={true} />
+              <Message />
+              <Message own={true} />
+              <Message />
+            </div>
+            <div className="chatBoxBottom">
+              <textarea
+                className="chatMessageInput"
+                placeholder="write something..."
+                //onChange={(e) => setNewMessage(e.target.value)}
+                /*value={newMessage}*/></textarea>
+              <button className="chatSubmitButton" onClick={console.log()}>
+                Send
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="chatOnline">
+          <div className="chatOnlineWrapper">
+            <ChatOnline/>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
